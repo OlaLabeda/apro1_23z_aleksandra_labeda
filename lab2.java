ec968b7a3e842b506ef1d2da3ca364ea3a41d69c
alabeda1 <01178554@pw.edu.pl>
2023-10-16 22:46:54 +0200

    sformatowany i poprawiony program hello world

--- lab2.java
+++ lab2.java
@@ -1,1 +1,5 @@
-public class lab2 { public static void main(String args[]) { System.out.println(Hello world); } }
+public class lab2 {
+     public static void main(String args[]) { 
+        System.out.println("Hello world"); 
+    } 
+}
