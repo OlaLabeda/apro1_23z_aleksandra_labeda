3f85ffec362635001ef497985ec1985cfb65710c
alabeda1 <aleksandra.labeda.stud@pw.edu.pl>
2023-10-23 22:46:41 +0200

    zadanie 1. zmiany

--- src/lab2/lab2.java
+++ src/lab2/lab2.java
@@ -1,7 +1,24 @@
 package lab2;
-
+import java.util.Scanner;
 public class lab2 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        // Tworzenie obiektu Scanner do odczytu danych z konsoli
+        Scanner scanner = new Scanner(System.in);
+
+        // Prośba o wprowadzenie imienia
+        System.out.print("Podaj swoje imię: ");
+        String imie = scanner.nextLine();
+
+        // Prośba o wprowadzenie pseudonimu
+        System.out.print("Podaj swoj pseudonim: ");
+        String pseudonim = scanner.nextLine();
+
+        System.out.println("Hello world\n" + "imie: \n" +imie
+                + "\npseudonim: " + pseudonim);
+
+        // Zamykanie obiektu Scanner
+        scanner.close();
     }
 }
