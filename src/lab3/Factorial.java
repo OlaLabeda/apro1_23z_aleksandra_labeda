7a2ffd742cb897faedf2d57d809d0d054ee5ac78
alabeda1 <aleksandra.labeda.stud@pw.edu.pl>
2023-10-23 17:37:32 +0200

    silnia, rozwiazanie

--- src/lab3/Factorial.java
+++ src/lab3/Factorial.java
@@ -4,7 +4,10 @@ public class Factorial {
     public static void main(String[] args) {
         short N = 4;
         int result = 1;
-        /* Write your code here */
+        while (N > 1) {
+            result *= N;
+            N--;
+        }
         System.out.println(result);
     }
 }
