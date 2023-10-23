a1b6e23820a26f87eca3b1993bad3a1a0420ffe1
alabeda1 <aleksandra.labeda.stud@pw.edu.pl>
2023-10-23 22:50:19 +0200

    zadanie vovels to z

--- src/lab2/lab2.java
+++ src/lab2/lab2.java
@@ -10,15 +10,28 @@ public class lab2 {
         // Prośba o wprowadzenie imienia
         System.out.print("Podaj swoje imię: ");
         String imie = scanner.nextLine();
+        imie = vovels_to_z(imie);
 
         // Prośba o wprowadzenie pseudonimu
         System.out.print("Podaj swoj pseudonim: ");
         String pseudonim = scanner.nextLine();
+        pseudonim = vovels_to_z(pseudonim);
 
         System.out.println("Hello world\n" + "imie: \n" +imie
                 + "\npseudonim: " + pseudonim);
+        System.out.println("Hello world\n" + "imie:" +imie + "\npseudonim: "
+                + pseudonim );
 
         // Zamykanie obiektu Scanner
         scanner.close();
+    }
+
+    public static String vovels_to_z (String tekst) {
+        // Zamienia wszystkie samogłoski na "z" (zarówno małe, jak i duże litery)
+        //replaceAll - szuka wszystkich wystapien wzorca w tekscie
+        //"[aeiouAEIOU]" <= wyrazenie regularne, znaki jakie maja zostac potencjalnie zamienione
+        // [] <=zestaw znakow
+        tekst = tekst.replaceAll("[aeiouyAEIOUY]", "z");
+        return tekst;
     }
 }
