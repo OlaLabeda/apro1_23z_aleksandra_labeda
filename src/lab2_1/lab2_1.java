edef8ed42ca6a970ee80e392facbddfdacd8a98f
alabeda1 <aleksandra.labeda.stud@pw.edu.pl>
2023-10-23 22:10:33 +0200

    kolejnosc zmieniona i slowa odwracane

--- src/lab2_1/lab2_1.java
+++ src/lab2_1/lab2_1.java
@@ -9,10 +9,12 @@ public class lab2_1 {
         // Prośba o wprowadzenie imienia
         System.out.print("Podaj swoje imię: ");
         String imie = scanner.nextLine();
+        imie = odwrocSlowa(imie);
 
         // Prośba o wprowadzenie pseudonimu
         System.out.print("Podaj swoj pseudonim: ");
         String pseudonim = scanner.nextLine();
+        pseudonim = odwrocSlowa(pseudonim);
 
         System.out.println("Hello world\n" + "pseudonim: " +
                 pseudonim + "\nimie: " +imie);
@@ -20,4 +22,24 @@ public class lab2_1 {
         // Zamykanie obiektu Scanner
         scanner.close();
     }
+    public static String odwrocSlowa(String tekst) {
+        //utorzenie pomocniczej tablicy znakow
+        char[] tablicaZnakow = tekst.toCharArray();
+        //uwtorzenie znacznikow poruszajacyh sie po tekscie
+        //ktory ma zostac odwrocony
+        int start = 0;
+        int end = tekst.length() - 1;
+
+        //petla przechodzaca po znakach tekstu odwracajaca ich kolejnosc
+        while (start < end) {
+            char temp = tablicaZnakow[start];
+            tablicaZnakow[start] = tablicaZnakow[end];
+            tablicaZnakow[end] = temp;
+            start++;
+            end--;
+        }
+
+        return new String(tablicaZnakow);
+    }
+
 }
