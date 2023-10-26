3681c93dc607fc82d22dc95addb0ce56920fd0b7
alabeda1 <aleksandra.labeda.stud@pw.edu.pl>
2023-10-26 13:25:48 +0200

    fabryka wersja poprawiona

--- src/lab4/Fabryka.java
+++ src/lab4/Fabryka.java
@@ -3,6 +3,20 @@ package lab4;
 import java.util.Scanner;
 
 public class Fabryka {
+    public static int calculateNumberOfDays(int jednostkiTowaru, int liczbaMaszyn, int[] szybkoscPracy) {
+        int sumaTowarow = 0;
+        int liczbaDni = 0;
+        while (sumaTowarow < jednostkiTowaru) {
+            liczbaDni++;
+            for (int i = 0; i < liczbaMaszyn; i++) {
+                if (liczbaDni % szybkoscPracy[i] == 0) {
+                    sumaTowarow++;
+                }
+            }
+        }
+        return liczbaDni;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int jednostkiTowaru = scanner.nextInt();
@@ -11,17 +25,8 @@ public class Fabryka {
         for (int i = 0; i < liczbaMaszyn; i++) {
             szybkoscPracy[i] = scanner.nextInt();
         }
-
-        int sumaTowarow = 0;
-        int liczbaDni = 0;
-        while (sumaTowarow < jednostkiTowaru) {
-            liczbaDni++;
-            for (int i = 0; i < liczbaMaszyn; i++){
-                if (liczbaDni % szybkoscPracy[i] == 0) {
-                    sumaTowarow++;
-                }
-            }
-        }
+        int liczbaDni = calculateNumberOfDays(jednostkiTowaru, liczbaMaszyn, szybkoscPracy);
         System.out.println(liczbaDni);
     }
+
 }
