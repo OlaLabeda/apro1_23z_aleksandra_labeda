c4be41306f6b6e257d7ad0fd52d0bf8099c53924
alabeda1 <aleksandra.labeda.stud@pw.edu.pl>
2023-11-06 19:32:35 +0100

    fabryka i machine - utworzenie klas

---
+++ src/lab5/Fabryka.java
@@ -0,0 +1,37 @@
+package lab5;
+
+import java.util.Scanner;
+
+/**
+ * @author Aleksandra Łabęda,
+ *
+ */
+
+public class Fabryka {
+    public static int calculateNumberOfDays(int jednostkiTowaru, int liczbaMaszyn, int[] szybkoscPracy) {
+        int sumaTowarow = 0;
+        int liczbaDni = 0;
+        while (sumaTowarow < jednostkiTowaru) {
+            liczbaDni++;
+            for (int i = 0; i < liczbaMaszyn; i++) {
+                if (liczbaDni % szybkoscPracy[i] == 0) {
+                    sumaTowarow++;
+                }
+            }
+        }
+        return liczbaDni;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int jednostkiTowaru = scanner.nextInt();
+        int liczbaMaszyn = scanner.nextInt();
+        int[] szybkoscPracy = new int[liczbaMaszyn];
+        for (int i = 0; i < liczbaMaszyn; i++) {
+            szybkoscPracy[i] = scanner.nextInt();
+        }
+        int liczbaDni = calculateNumberOfDays(jednostkiTowaru, liczbaMaszyn, szybkoscPracy);
+        System.out.println(liczbaDni);
+    }
+
+}
