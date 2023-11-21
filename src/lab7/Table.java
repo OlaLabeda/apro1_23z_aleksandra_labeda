fa4ff78a61dc152f64bfcb0a227468f4a780ff16
alabeda1 <aleksandra.labeda.stud@pw.edu.pl>
2023-11-21 13:49:48 +0100

    modified docummentation

--- src/lab7/Table.java
+++ src/lab7/Table.java
@@ -1,4 +1,7 @@
 package lab7;
+
+import java.util.Objects;
+
 /**
  * @author Aleksandra Łabęda,
  * Bartosz Czerwiński
@@ -39,8 +42,20 @@ public class Table extends Furniture {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Table table = (Table) o;
+        return legNumber == table.legNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(legNumber);
+    }
+
+    @Override
     public String toString() {
-        return "Stol " + super.getDesccription() + " z nastepujaca iloscia nog: " +
-                 legNumber;
+        return "Stol " + super.getDesccription();
     }
 }
