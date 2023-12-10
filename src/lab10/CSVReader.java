f9b0de58fe88c25308c126105a468c87f7776627
alabeda1 <aleksandra.labeda.stud@pw.edu.pl>
2023-12-10 14:54:47 +0100

    try with resources change

---
+++ src/lab10/CSVReader.java
@@ -0,0 +1,50 @@
+package lab10;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CSVReader {
+    private List<String[]> rows = new ArrayList<>();
+    private String fileName;
+    private String separator;
+
+    public CSVReader(String fileName, String separator) {
+        this.fileName = fileName;
+        this.separator = separator;
+    }
+
+    public List<String[]> read() throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                rows.add(line.split(separator));
+            }
+        }
+        return rows;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        rows.forEach(row -> {
+            sb.append(String.join(":", row));
+            sb.append(System.lineSeparator());
+        });
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        CSVReader csvReader = new CSVReader("oscar_age_male.csv", ",");
+        try {
+            List<String[]> data = csvReader.read();
+            System.out.println(data);
+            System.out.println(csvReader);
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Obsługa wyjątku w main
+        }
+    }
+}
